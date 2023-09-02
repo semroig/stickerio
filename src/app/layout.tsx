e48b59da75b3266aaef819c441e277366ac8f5f7
Sem Gabriel Roig <sem.roig@gmail.com>
2023-09-02 20:30:03 -0300

    saco tabindex y armo cards de profile page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,10 +60,10 @@ export default function RootLayout({
                     <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
                   </button>
                 </div>
-                <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+                <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button">
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-0">Your Profile</a>
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-1">Settings</a>
+                  <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-2">Sign out</a>
                 </div>
               </div>
             </div>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -11,25 +11,35 @@ import {
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-row justify-center">
-        <div className="m-8">
+      <div className="flex flex-row justify-center px-20 mt-5">
+        <div className="m-6 basis-1/3">
           <p className="font-semibold text-2xl">Mis datos</p>
-          <Card className="mt-3 w-[350px]">
+          <Card className="mt-4 pt-5">
             <CardContent>
-              contentt
+              <p className="font-semibold text-xl">Nombre</p>
+              <p className="text-lg">Sem Gabriel Roig</p>
+              <p className="font-semibold text-xl mt-2">Email</p>
+              <p className="text-lg">sem.roig@gmail.com</p>
+              <p className="font-semibold text-xl mt-2">Teléfono</p>
+              <p className="text-lg">+54 9 11 4936 0562</p>
             </CardContent>
-            <CardFooter className="flex justify-end">
+            <CardFooter className="flex justify-center">
               <Button variant="outline">Editar</Button>
             </CardFooter>
           </Card>
         </div>
-        <div className="m-8">
-          <p className="font-semibold text-2xl">Mi dirección</p>
-          <Card className="mt-3 w-[350px]">
+        <div className="m-6 basis-1/3">
+          <p className="font-semibold text-2xl">Dirección de envío</p>
+          <Card className="mt-4 pt-5">
             <CardContent>
-              contentt
+              <p className="font-semibold text-xl">Dirección</p>
+              <p className="text-lg">Maipú 233</p>
+              <p className="font-semibold text-xl mt-2">Localidad</p>
+              <p className="text-lg">General Pacheco</p>
+              <p className="font-semibold text-xl mt-2">Provincia</p>
+              <p className="text-lg">Buenos Aires</p>
             </CardContent>
-            <CardFooter className="flex justify-end">
+            <CardFooter className="flex justify-center">
               <Button variant="outline">Editar</Button>
             </CardFooter>
           </Card>

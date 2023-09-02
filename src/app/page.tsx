99d9c9ad301145fa7455204ac8364c5e52c7c43b
Sem Gabriel Roig <sem.roig@gmail.com>
2023-09-02 20:31:38 -0300

    fix prop en main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div class="grid h-screen place-items-center">
+    <div className="grid h-screen place-items-center">
       <div>
-        <h1 class="text-2xl">Landing page</h1>
+        <h1 className="text-2xl">Landing page</h1>
         <Button>Click me</Button>
       </div>
     </div>

cfe9707ab284070299abb886f4747c259bb6feb8
Sem Gabriel Roig <sem.roig@gmail.com>
2023-09-14 22:14:56 -0300

    supabase funcionaa

--- src/app/catalogo/page.tsx
+++ src/app/catalogo/page.tsx
@@ -1,10 +1,20 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+  const { data: categories } = await supabase.from("Category").select();
+
   return (
     <main>
-        <h1>Catalogo</h1>
+      <h1>Catalogo</h1>
       <Button>Click me</Button>
+      <ul className="my-auto text-foreground">
+        {categories?.map((category) => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+       </ul>
     </main>
   )
 }

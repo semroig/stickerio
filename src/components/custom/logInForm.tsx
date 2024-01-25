aa368ccd659f956f8c97061d1eb8cf1d29dab10a
Sem Gabriel Roig <sem.roig@gmail.com>
2024-01-25 17:20:15 -0300

    auth sin funcionar

--- src/components/custom/logInForm.tsx
+++ src/components/custom/logInForm.tsx
@@ -1,50 +1,75 @@
+'use client'
+
+import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
 export default function LogInForm() {
-  return (
-    <form className="space-y-6" action="#" method="POST">
-        <div>
-        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-        <div className="mt-2">
-            <Input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                required
-            ></Input>
-        </div>
-        </div>
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const router = useRouter();
 
-        <div>
-        <div className="flex items-center justify-between">
-            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-            <div className="text-sm">
-            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+    const supabase = createClientComponentClient();
+
+    const handleSignIn = async () => {
+        await supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+        router.refresh();
+
+        // Refresh data states
+        setEmail('');
+        setPassword('');
+    }
+
+    return (
+        <form className="space-y-6">
+            <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+            <div className="mt-2">
+                <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                ></Input>
             </div>
-        </div>
-        <div className="mt-2">
-            <Input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            ></Input>
-        </div>
-        </div>
+            </div>
 
-        <div>
-        <Button
-            type="submit"
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-            Sign in
-        </Button>
-        </div>
-    </form>
-  )
+            <div>
+            <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                <div className="text-sm">
+                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                </div>
+            </div>
+            <div className="mt-2">
+                <Input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                ></Input>
+            </div>
+            </div>
+
+            <div>
+            <Button
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={handleSignIn}
+            >
+                Sign in
+            </Button>
+            </div>
+        </form>
+    )
 }
--- src/components/custom/signUpForm.tsx
+++ src/components/custom/signUpForm.tsx
@@ -1,9 +1,41 @@
+'use client'
+
+import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
 export default function SignUpForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+  const supabase = createClientComponentClient();
+
+  const handleSignUp = async () => {
+    const { data, error} = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            emailRedirectTo: `${location.origin}/auth/callback`,
+            data: {
+              first_name: name
+            }
+        }
+    })
+
+    router.refresh();
+
+    // Refresh data states
+    setName('');
+    setEmail('');
+    setPassword('');
+}
+
   return (
-    <form className="space-y-6" action="#" method="POST">
+    <form className="space-y-6">
       <div>
           <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Nombre</label>
           <div className="mt-2">
@@ -14,6 +46,7 @@ export default function SignUpForm() {
                   autoComplete="name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   required
+                  onChange={(e) => setName(e.target.value)}
               ></Input>
           </div>
       </div>
@@ -28,6 +61,7 @@ export default function SignUpForm() {
                 autoComplete="email"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 required
+                onChange={(e) => setEmail(e.target.value)}
             ></Input>
         </div>
       </div>
@@ -40,16 +74,17 @@ export default function SignUpForm() {
                 name="password"
                 type="password"
                 autoComplete="current-password"
-                required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                required
+                onChange={(e) => setPassword(e.target.value)}
             ></Input>
         </div>
       </div>
 
       <div>
         <Button
-            type="submit"
             className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={handleSignUp}
         >
           Registrarse
         </Button>

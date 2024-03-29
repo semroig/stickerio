f10e5bf0a1ed7ea873b2c333cf356cbecbfe3738
Sem Gabriel Roig <sem.roig@gmail.com>
2024-03-29 13:58:09 -0300

    pagination working

--- src/app/catalogo/page.tsx
+++ src/app/catalogo/page.tsx
@@ -1,42 +1,59 @@
-import { cookies } from "next/headers";
-import { createServerClient } from '@supabase/ssr'
+import createSupabaseServerClient from "@/lib/supabase/server";
 
 import Navbar from "@/components/custom/landing/navbar"
 import Footer from "@/components/custom/landing/footer"
 import Tarjeta from "@/components/custom/tarjeta";
 import CategoriesSection from "@/components/custom/categoriesSection";
 
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
 export default async function Home({ searchParams }: any) {
   // Inicializo cliente de supabase
-  const cookieStore = cookies()
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        },
-      },
-    }
-  )
+  const supabase = await createSupabaseServerClient();
 
   // Traigo todas las categorias
   const { data: categories } = await supabase.from("Collection").select();
 
+  // Reviso si hay paginacion
+  let pagina = 1;
+  if (searchParams.page && searchParams.page >= 1) pagina = parseInt(searchParams.page);
+
   // Traigo los prods segun search params de categories seleccionados
   let resp;
   if (searchParams.category){
-    let filteringCategories = searchParams.category.split('-');
+    const filteringCategories = searchParams.category.split('-');
     resp = await supabase
     .from("Product")
-    .select("*")
-    .in('collection_id', filteringCategories);
+    .select("*", { count: 'exact' })
+    .in('collection_id', filteringCategories)
+    .range((pagina - 1) * 12, pagina * 12 - 1)
   }
   else {
     resp = await supabase
     .from("Product")
-    .select("*");
+    .select("*", { count: 'exact' })
+    .range((pagina - 1) * 12, pagina * 12 - 1)
+  }
+
+  const pagItems = [];
+  for (let i = 0; i < resp.count! / 12; i++) {
+      pagItems.push(
+        <PaginationItem key={i}>
+          <PaginationLink 
+            href={`/catalogo?category=${searchParams.category ? searchParams.category : ''}&page=${i + 1}`}
+            isActive={i + 1 == pagina}
+          >
+            {i + 1}
+          </PaginationLink>
+        </PaginationItem>
+      );
   }
 
   return (
@@ -50,13 +67,39 @@ export default async function Home({ searchParams }: any) {
         </div>
         <div className="basis-2/3">
           <div className="flex flex-wrap">
-
             {resp.data?.map((product) => (
               <div key={product.id}>
                 <Tarjeta record={product}></Tarjeta>
               </div>
             ))}
           </div>
+
+          {/* Pagination */}
+          <Pagination className="mt-7">
+            <PaginationContent>
+              {/* Previous button */}
+              {pagina != 1 && (
+                <PaginationItem>
+                  <PaginationPrevious
+                    href={`/catalogo?category=${searchParams.category ? searchParams.category : ''}&page=${pagina - 1}`}
+                  />
+                </PaginationItem>
+              )}
+
+              {/* Render all buttons */}
+              {pagItems}
+
+              {/* Next button */}
+              {pagina != Math.ceil(resp.count! / 12) && (
+                <PaginationItem>
+                  <PaginationNext
+                    href={`/catalogo?category=${searchParams.category ? searchParams.category : ''}&page=${pagina + 1}`}
+                  />
+                </PaginationItem>
+              )}
+            </PaginationContent>
+          </Pagination>
+
         </div>
       </div>
 

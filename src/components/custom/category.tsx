a9e79fc671af75a04e1d4c0b1e08f8178c9f872b
Sem Gabriel Roig <sem.roig@gmail.com>
2024-04-01 11:19:50 -0300

    comento check de categoria

--- src/components/custom/category.tsx
+++ src/components/custom/category.tsx
@@ -6,7 +6,7 @@ export default function Category({ record, addFilter, removeFilter, categoriaChe
     return (
         <div className="my-2 flex items-center space-x-2">
             <Checkbox
-                checked={categoriaChecked}
+                // checked={categoriaChecked}
                 id="category"
                 onCheckedChange={(e) => e ? addFilter(record.id) : removeFilter(record.id)}
             />

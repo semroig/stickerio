e5200e9cecc2c43f1cd8df21a7c1541e453abde6
Sem Gabriel Roig <sem.roig@gmail.com>
2024-04-13 19:39:07 -0300

    agrego alert en cpag de prod

--- src/app/producto/[id]/page.tsx
+++ src/app/producto/[id]/page.tsx
@@ -9,9 +9,12 @@ import Footer from "@/components/custom/landing/footer"
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+
 import ProductInputSection from "@/components/custom/productInputSection";
 
 import { ArrowLeft } from 'lucide-react';
+import { Check, ChevronsUpDown, AlertCircle } from "lucide-react"
 
 export default async function Home({ params }: any) {
     // Verifico si esta logueado
@@ -57,6 +60,15 @@ export default async function Home({ params }: any) {
             <p className="font-medium text-4xl lg:text-5xl text-verde">{ product![0].name }</p>
             <p className="text-xl mt-3 text-gris font-light">{ product![0].description }</p>
             {/* <p className="font-light text-5xl my-10 ">$ 200</p> */}
+
+            {!sessionData.session && (
+              <Alert className="mt-20 p-7">
+                  <AlertTitle>Hola! :)</AlertTitle>
+                  <AlertDescription>
+                      Para poder agregar este sticker a tu carrito tenes que ingresar a tu cuenta.
+                  </AlertDescription>
+              </Alert>
+            )}
 
             {sessionData.session && <ProductInputSection record={product![0]} userSessionData={ sessionData.session } />}
 
